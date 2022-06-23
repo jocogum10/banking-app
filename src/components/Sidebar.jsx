0f2d4036962967dc31acceaaef7c4f1185bbd536
Joco Gum <jocogum10@gmail.com>
2022-06-23 21:03:44 +0800

    add sidebar contents

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,14 @@
 function Sidebar () {
     return (
         <div className="bg-emerald-700 col-span-2 row-start-2 row-span-full">
-            <h1>Home</h1>
-            <h1>Add</h1>
-            <h1>Profile</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Home</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">My Account</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Transfer Money</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Payments / Load</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Investments</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Account Maintenance</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Health and Life Protection</h1>
+            <h1 className="p-2 m-1 bg-emerald-300 rounded shadow-lg">Other Services</h1>
         </div>
         
     )
